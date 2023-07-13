8e894e2f6ea9bdaaa0aee2778b7d666185efc8cb
kravchenko-alexandr-otus <vampire002@rambler.ru>
2023-07-13 09:57:06 +0300

    added mock-fs

--- index.test.js
+++ index.test.js
@@ -1,35 +1,50 @@
 const {picture, main_tree} = require('./index')
 const mod_path = require('path')
-const argv = require('minimist')(process.argv.slice(2))
+const mock = require('mock-fs')
+
+beforeEach(()=> {
+    mock({
+        '/some/path/to/file':{
+            'content.txt': 'some text',
+            'empty_dir':{}
+        },
+    })
+})
+
+
+afterEach(() => {
+    mock.restore()
+})
 
 test('Test top/max depth value', ()=>{
-    expect(main_tree('./node_modules', 1000001))
+    expect(main_tree('/some', 1000001))
     .toEqual("TOO MANY FOLDERS I DON'T WANT TO DO IT...")
 })
 
 test('Test min/botom depth value', ()=>{
-    expect(main_tree('./node_modules', -1))
+    expect(main_tree('/some', -1))
     .toEqual("DEPTH SHOULD BE POSITIVE")
 })
 
 test('Test ability to output', ()=> {
-    expect(main_tree('./node_modules', 0))
-    .toEqual("node_modules")
+    expect(main_tree('/some', 0))
+    .toEqual("some")
 })
 
 test('Test ability to picture filesystem tree', ()=>{
-    expect(main_tree('./folder', 1))
+    expect(main_tree('/some', 1))
     .toEqual(
-`folder
-|---child_folder
-|___file.text`)
+`some
+|___path`)
 })
 
 test('Test ability to picture more deep tree', ()=>{
-    expect(main_tree('./folder', 2))
+    expect(main_tree('/some', 4))
     .toEqual(
-`folder
-|---child_folder
-|    |___child_file.txt
-|___file.text`)
+`some
+|___path
+    |___to
+        |___file
+            |---content.txt
+            |___empty_dir`)
 })
